48d562dc859eb22798daf853dd171351f8c5eb6c
Wiktor Jarka <wiktorj@gmail.com>
2025-10-14 15:52:27 +0000

    chore: add camel_case / snake_case converters

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const SNAKE_CASE_SEGMENT = /_([a-zA-Z0-9])/g;
+const CAMEL_CASE_BOUNDARY = /([a-z0-9])([A-Z])/g;
+
+export function snakeToCamel(value: string): string {
+  return value.replace(SNAKE_CASE_SEGMENT, (_match, group: string) => group.toUpperCase());
+}
+
+export function camelToSnake(value: string): string {
+  return value.replace(CAMEL_CASE_BOUNDARY, "$1_$2").toLowerCase();
 }
